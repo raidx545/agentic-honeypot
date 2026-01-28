4de38e04a93271cc8aa1da555885b20c68edfb2d
Raj Porwl <raazporwal6@gmail.com>
2026-01-29 02:20:11 +0530

    scam_detector added

---
+++ app/core/engagement_agent.py
@@ -0,0 +1,6 @@
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="AIzaSyBeodY4tx9lUBJeX45sda6zEZ5yj2DrSkM",
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
---
+++ app/core/scam_detector.py
@@ -0,0 +1,54 @@
+from openai import OpenAI
+from pydantic import BaseModel
+import json
+class ScamRequest(BaseModel):
+    message: str
+
+def is_scam(message:str) -> True:
+
+    client = OpenAI(
+        api_key = "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",
+        base_url="https://openrouter.ai/api/v1"
+    )
+
+    SCAM_KEYWORDS = [
+      "urgent", "account blocked", "verify now",
+      "send money", "upi", "otp", "kyc"
+    ]
+    SYSTEM_PROMPT = f"""You are a scam detection system.
+    Rules:
+    - If the message contains ANY of these keywords:
+    {", ".join(SCAM_KEYWORDS)} 
+    classify it as a scam.
+    - Otherwise classify it as non-scam.
+    Respond ONLY in valid JSON. 
+    No extra text.
+    JSON format:
+    {{
+    "is_scam": true or false,
+    "confidence": number between 0 and 1
+    }}
+"""
+    
+    response = client.chat.completions.create(
+        model="openai/gpt-oss-20b",
+        messages=[{"role":"system" , "content":f"{SYSTEM_PROMPT}"},
+                  {
+                      "role":"user" , "content" : f"{message}"
+                  }
+        ]
+    )
+
+    raw_output = response.choices[0].message.content.strip()
+
+    try :
+        result = json.loads(raw_output)
+    except json.JSONDecodeError:
+        raise ValueError("Wrong JSON format from LLM")
+    
+    return result["is_scam"]
+
+# print(is_scam("Sir your account has been blocked , send me money to this number"))
+
+if __name__ == "__main__":
+    is_scam()
